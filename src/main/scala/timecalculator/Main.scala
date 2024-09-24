6c211794baeda30d3e0427bd9b6e5cc44f6ec976
zcBruAll <allan.brunner@zeacold.com>
2024-09-24 21:37:55 +0200

    Created the main html page

--- src/main/scala/timecalculator/Main.scala
+++ src/main/scala/timecalculator/Main.scala
@@ -5,8 +5,8 @@ import org.scalajs.dom
 
 @main
 def TimeCalculator(): Unit = {
-    dom.document.querySelector("#app").innerHTML = """
-        <h1>Hello ScalaJS!</h1>
+    /*dom.document.querySelector("#app").innerHTML = """
+        <h1>Hello Scala!</h1>
         <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-    """
+    """*/
 }
